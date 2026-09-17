fbb1046d4305dee558b07071e0cd2ac9f85e8fb6
Marek Hlava <headsvk@gmail.com>
2026-09-17 12:24:27 +0100

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun trap(height: IntArray): Int {
+        if (height.size < 3) {
+            return 0
+        }
+
+        var l = 0
+        var r = height.size - 1
+        var maxL = height[l]
+        var maxR = height[r]
+        var sum = 0
+
+        while (l < r) {
+            if (maxL <= maxR) {
+                l++
+                maxL = maxOf(maxL, height[l])
+                sum += maxL - height[l]
+            } else {
+                r--
+                maxR = maxOf(maxR, height[r])
+                sum += maxR - height[r]
+            }
+        }
+        return sum
+    }
+}
