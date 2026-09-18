4358b4d7951f363798277088f7d566a1552ddad0
Marek Hlava <headsvk@gmail.com>
2026-09-18 16:30:20 +0100

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.kt
@@ -0,0 +1,24 @@
+class TimeMap() {
+
+    private data class Item(val value: String, val timestamp: Int)
+
+    private val map = mutableMapOf<String, MutableList<Item>>()
+
+    fun set(key: String, value: String, timestamp: Int) {
+        val items = map.getOrPut(key) { mutableListOf<Item>() }
+        items.add(Item(value, timestamp))
+    }
+
+    fun get(key: String, timestamp: Int): String {
+        val items = map[key] ?: return ""
+
+        val index = items.binarySearchBy(timestamp) { it.timestamp }
+
+        return if (index >= 0) {
+            items[index].value
+        } else {
+            val normalizedIndex = -(index + 2)
+            if (normalizedIndex < 0) "" else items[normalizedIndex].value
+        }
+    }
+}
